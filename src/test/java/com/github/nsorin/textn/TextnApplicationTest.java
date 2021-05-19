d2c49f47584c9863d01adb0baf584d6117e2799f
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2021-05-19 08:58:09 +0200

    Pom update + intelliJ files included in git

--- src/test/java/com/github/nsorin/textn/TextnApplicationTest.java
+++ src/test/java/com/github/nsorin/textn/TextnApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TextnApplicationTests {
+class TextnApplicationTest {
 
 	@Test
 	void contextLoads() {
