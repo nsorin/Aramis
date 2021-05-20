b9af771e2b2cfae6a54edc0b068fbe7063ce0f3d
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2021-05-20 21:05:15 +0200

    Updated maven and module config to allow run tests in cl

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,4 @@
-module textn {
+open module textn {
     requires javafx.controls;
     requires javafx.fxml;
-
-    exports com.github.nsorin.textn.ui;
-    exports com.github.nsorin.textn.ui.controller;
-    exports com.github.nsorin.textn.service;
-
-    opens com.github.nsorin.textn to javafx.graphics;
-    opens com.github.nsorin.textn.ui.controller to javafx.fxml;
 }
