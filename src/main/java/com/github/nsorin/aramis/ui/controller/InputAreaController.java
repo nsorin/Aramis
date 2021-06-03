30fcbfea16694e85846b610324ec6856daeb9427
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2021-06-03 15:26:21 +0200

    Used property listener instead of key event

--- src/main/java/com/github/nsorin/aramis/ui/controller/InputAreaController.java
+++ src/main/java/com/github/nsorin/aramis/ui/controller/InputAreaController.java
@@ -6,21 +6,23 @@ import com.github.nsorin.aramis.model.event.TextContentUpdated;
 import com.github.nsorin.aramis.observer.OnEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
-import javafx.scene.input.KeyEvent;
 
 public class InputAreaController {
 
     @FXML
     private TextArea inputArea;
 
+    @FXML
+    public void initialize() {
+        inputArea.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            applicationState.setSaved(false);
+            applicationState.getTextContent().setContent(newValue);
+            inputArea.requestFocus();
+        });
+    }
+
     @Injectable
     private ApplicationState applicationState;
-
-    public void onKeyTyped(KeyEvent keyEvent) {
-        String inputText = inputArea.getText();
-        applicationState.setSaved(applicationState.getTextContent().getContent().equals(inputText));
-        applicationState.getTextContent().setContent(inputText);
-    }
 
     @OnEvent
     public void onTextContentUpdated(TextContentUpdated event) {
