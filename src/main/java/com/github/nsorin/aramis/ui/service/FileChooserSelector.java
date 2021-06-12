eb967ac09c75688e5641beeec42b6d3cd9d324ad
Nicolas Sorin <nicolas.sorin1996@gmail.com>
2021-06-12 18:39:24 +0200

    Added extension filters in FileChooserSelector

--- src/main/java/com/github/nsorin/aramis/ui/service/FileChooserSelector.java
+++ src/main/java/com/github/nsorin/aramis/ui/service/FileChooserSelector.java
@@ -7,20 +7,30 @@ import java.io.File;
 
 public class FileChooserSelector implements FileSelector {
 
-    public static final String OPEN_FILE_HEADER = "Open text file";
-    public static final String SAVE_FILE_HEADER = "Open text file";
+    public static final String OPEN_FILE_HEADER = "Open File";
+    public static final String SAVE_FILE_HEADER = "Save File";
+    public static final String AXML_FILTER_NAME = "AXML Files";
+    public static final String AXML_FILTER_MATCHER = "*.axml";
+    public static final String ALL_FILTER_NAME = "All Files";
+    public static final String ALL_FILTER_MATCHER = "*";
 
     @Override
     public File selectFileToOpen(Window window) {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle(OPEN_FILE_HEADER);
-        return fileChooser.showOpenDialog(window);
+        return getFileChooser(OPEN_FILE_HEADER).showOpenDialog(window);
     }
 
     @Override
     public File selectFileToSave(Window window) {
+        return getFileChooser(SAVE_FILE_HEADER).showSaveDialog(window);
+    }
+
+    private FileChooser getFileChooser(String header) {
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle(SAVE_FILE_HEADER);
-        return fileChooser.showSaveDialog(window);
+        fileChooser.setTitle(header);
+        FileChooser.ExtensionFilter axmlFilter = new FileChooser.ExtensionFilter(AXML_FILTER_NAME, AXML_FILTER_MATCHER);
+        FileChooser.ExtensionFilter allFilter = new FileChooser.ExtensionFilter(ALL_FILTER_NAME, ALL_FILTER_MATCHER);
+        fileChooser.getExtensionFilters().add(axmlFilter);
+        fileChooser.getExtensionFilters().add(allFilter);
+        return fileChooser;
     }
 }
